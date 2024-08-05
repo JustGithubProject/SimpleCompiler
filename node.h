91b6e084628ccdfb10207c4d5415e88630e081cc
JustGithubProject <Kropivadanil@gmail.com>
2024-08-05 18:41:09 +0300

    Defined extra AST items

--- node.h
+++ node.h
@@ -55,5 +55,79 @@ public:
 
 class NMethodCall : public NExpression
 {
-
+public:
+    const NIdentifier& id;
+    ExpressionList arguments;
+    NMethodCall(const NIdentifier& id, ExpressionList& arguments) : id(id), arguments(arguments) {}
+    NMethodCall(const NIdentifier& id) : id(id) {}
+    virtual llvm::Value* codeGen(CodeGenContext& context);
 };
+
+
+class NBinaryOperator : public NExpression
+{
+public:
+    int op;
+    NExpression& lhs;
+    NExpression& rhs;
+    NBinaryOperator(NExpression& lhs, int op, NExpression& rhs) : lhs(lhs), op(op), rhs(rhs) {}
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+
+class NAssignment : public NExpression
+{
+public:
+    NIdentifier& lhs;
+    NExpression& rhs;
+    NAssignment(NIdentifier& lhs, NIdentifier& rhs) : lhs(lhs), rhs(rhs) {}
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+
+class NBlock : public NExpression
+{
+public:
+    StatementList statements;
+    NBlock() {}
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+
+class NExpressionStatement : public NStatement
+{
+public:
+    NExpression& expression;
+    NExpressionStatement(NExpression& expression) : expression(expression) {}
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+
+class NVariableDeclaration : public NStatement
+{
+    const NIdentifier& type;
+    NIdentifier& id;
+    NExpression *assignmentExpr;
+
+    NVariableDeclaration(const NIdentifier& type, NIdentifier& id) 
+        : type(type), id(id) {}
+    NVariableDeclaration(const NIdentifier& type, NIdentifier& id, NExpression *assignmentExpr) :
+        type(type), id(id), assignmentExpr(assignmentExpr) {}
+    virtual llvm::Value* codeGen(CodeGenContext& context); 
+};
+
+
+class NFunctionDeclaration : public NStatement
+{
+public:
+    const NIdentifier& type;
+    const NIdentifier& id;
+    VariableList arguments;
+    NBlock& block;
+
+    NFunctionDeclaration(const NIdentifier& type, const NIdentifier& id, VariableList arguments, NBlock& block) :
+        type(type), id(id), arguments(arguments), block(block) {}
+    virtual llvm::Value* codeGen(CodeGenContext& context); 
+};
+
+
